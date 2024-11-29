ff5a5e3129b9d9af44379bdc91473b802607482f
José Ronaldo <dev.joseronaldo@gmail.com>
2024-11-29 10:36:39 -0300

    security: disable error messages

--- login/private/autoload.php
+++ login/private/autoload.php
@@ -1,5 +1,9 @@
 <?php
 
+// O comando abaixo desativa mensagens de erro de código no navegador
+// ativar isso em produção 
+//ini_set("display_errors", 1);
+
 require "cfg/dbconnect.php";
 require "functions.php";
 require "topmenu.php";
